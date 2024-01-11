c5933d6d79838f940a655d43ce913a810569e958
msantux <marcelo@santux.com>
2024-01-11 08:31:45 -0300

    11-Jan-2024

---
+++ 08_GoModAndDependencyManagement/48_ModularCodeDependencyMgmtGoGet/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/msantux/puppy"
+)
+
+func main() {
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module LearnHowToCodeGo
 
 go 1.21.5
+
+require github.com/msantux/puppy v0.0.0-20240111110407-8de5a9ca6a94
