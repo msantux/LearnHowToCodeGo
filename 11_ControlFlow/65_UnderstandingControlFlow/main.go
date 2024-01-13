1c426cfbd580c7c7aba72905320a4b081d248b30
msantux <marcelo@santux.com>
2024-01-13 07:43:52 -0300

    3-Jan-2024

---
+++ 11_ControlFlow/65_UnderstandingControlFlow/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// SEQUENCE
+	fmt.Println("This is the first statement to run")
+	fmt.Println("This is the second statement to run")
+	x := 40 // This is the third statement to run
+	y := 5  // This is the fourth statement to run
+	fmt.Printf("x = %v \t y = %v\n", x, y)
+}
---
+++ 11_ControlFlow/66_IfStatementAndComparisonOperators/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	// SEQUENCE
+	fmt.Println("This is the first statement to run")
+	fmt.Println("This is the second statement to run")
+	x := 40 // This is the third statement to run
+	y := 5  // This is the fourth statement to run
+	fmt.Printf("x = %v \t y = %v\n", x, y)
+
+	// CONDITIONAL
+	// if statement
+	// switch statement
+
+	if x < 42 {
+		fmt.Println("Less than the meaning of life")
+	}
+
+	if x < 42 {
+		fmt.Println("Less than the meaning of life")
+	} else {
+		fmt.Println("Greater than, or equal to, the meaning of life")
+	}
+
+	if x < 42 {
+		fmt.Println("Less than the meaning of life")
+	} else if x == 42 {
+		fmt.Println("Equal to, the meaning of life")
+	} else {
+		fmt.Println("Greater than the meaning of life")
+	}
+
+}
