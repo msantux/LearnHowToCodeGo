002f63fe29f096de86b18188fc97a77ca88f9639
msantux <marcelo@santux.com>
2024-02-22 11:53:36 -0300

    22-feb-2024

---
+++ 33_NinjaLevel12Exercises/Exercise01/dog/dog.go
@@ -0,0 +1,7 @@
+// Package dog implements functionality for understanding dogs
+package dog
+
+// Years converts human years to dog years (dogYears = 7 * humanYears)
+func Years(years int) int {
+	return years * 7
+}
---
+++ 33_NinjaLevel12Exercises/Exercise01/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"LearnHowToCodeGo/33_NinjaLevel12Exercises/Exercise01/dog"
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("5 human years = %d dog years\n", dog.Years(5))
+	fmt.Printf("7 human years = %d dog years\n", dog.Years(7))
+	fmt.Printf("10 human years = %d dog years\n", dog.Years(10))
+}
